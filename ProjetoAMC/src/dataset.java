f1612afb1bcb8080db75d9acb695d75f2e3f9ceb
miguelrfernandes <miguelfernandesfernandes10@gmail.com>
2021-04-29 10:30:16 +0100

    Criada a classe dataset e datasetLL

---
+++ ProjetoAMC/src/dataset.java
@@ -0,0 +1,34 @@
+
+public class dataset {
+	//Implementação com array de arrays não é possível pois é necessário alterar o tamanho
+	//do dataset (método add)
+	int data[][];
+	
+	public dataset(int[][] data) {
+		super();
+		this.data = data;
+	}
+
+	// Count:recebe uma lista de variáveis e valores destas e retorna o número
+	//de vezes que estas variáveis tomam simultaneamente os esses valores no dataset.
+	public int count(int[] vars, int[] val) {
+		int r = 0;
+		for (int[] row : data) {
+			boolean c = true;
+			for (int var : vars) {
+				if (row[var] != val[var]) c = false;
+			}
+			if (c) r++;
+		}
+		return r;
+	}
+	
+	// Add: adiciona um vetor ao dataset.
+	public void add(int[] v) {
+		data[0] = v;
+	}
+	
+	// Fiber: dado um valor da classe retorna a fibra (Dataset) associada
+	// a esse valor da classe.
+	// O que é uma fibra (Dataset)?
+}
---
+++ ProjetoAMC/src/datasetLL.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+
+public class datasetLL {
+	//Implementação com Listas Ligadas
+	LinkedList<int[]> data = new LinkedList<int[]>();
+	
+	public datasetLL() {
+		super();
+	}
+
+	// Count:recebe uma lista de variáveis e valores destas e retorna o número
+	//de vezes que estas variáveis tomam simultaneamente os esses valores no dataset.
+	public int count(int[] vars, int[] val) {
+		int r = 0;
+		for (int i = 0; i < data.size(); i++) {
+			boolean c = true;
+			for (int var : vars) {
+				if (data.get(i)[var] != val[var]) c = false;
+			}
+			if (c) r++;
+		}
+		return r;
+	}
+	
+	// Add: adiciona um vetor ao dataset.
+	public void add(int[] v) {
+		data.add(v);
+	}
+	
+	// Fiber: dado um valor da classe retorna a fibra (Dataset) associada
+	// a esse valor da classe.
+	// O que é uma fibra (Dataset)?
+}
