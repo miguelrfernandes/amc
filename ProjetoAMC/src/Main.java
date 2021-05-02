165ddb7194c13d038200bf714e976700dcf2c062
miguelrfernandes <miguelfernandesfernandes10@gmail.com>
2021-05-02 10:50:47 +0100

    Update Main.java

--- ProjetoAMC/src/Main.java
+++ ProjetoAMC/src/Main.java
@@ -4,6 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// Exemplo 1 — 10 medições, tamanha, cor, regularidade..., beningno (0) ou maligno (1)
+		int[][] exemplo1 = {{5,2,5,6,3,5,6,7,3,0},
+		{5,2,5,2,3,5,6,7,3,0},
+		{1,2,5,6,4,5,6,7,3,1},
+		{5,2,2,6,3,5,6,7,3,0}};
+		
+		// Exemplo 2 — 15 medições, tamanha, cor, regularidade..., carcinoma (0), adenoma (1), sarcoma (2)
+		int[][] exemplo2 = {{5,2,5,6,3,5,6,7,3,5,2,5,6,4,0},
+		{5,2,5,6,3,5,6,7,3,5,2,5,6,4,0},
+		{5,2,5,6,3,5,6,7,3,5,2,5,6,4,0},
+		{5,2,5,6,3,5,6,7,3,5,2,5,6,4,0}};
+		
 	}
 
 }
