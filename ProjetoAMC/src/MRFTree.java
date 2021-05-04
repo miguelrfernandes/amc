a4befb609167cc5ac6055333352d93eec189bd31
miguelrfernandes <miguelfernandesfernandes10@gmail.com>
2021-05-04 17:02:01 +0100

    mrftree já tem o phi!

--- ProjetoAMC/src/MRFTree.java
+++ ProjetoAMC/src/MRFTree.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class MRFTree {
 	// consideramos como no de partida o primeiro no (no 0)
@@ -9,23 +9,30 @@ public class MRFTree {
 	int[] D;
 	double delta = 0.2;
 	Dataset ds;
+	Dataset tfiber;
 	
 	// Método Construtor que recebe uma arvore (um grafo em forma de arvore), e um dataset e coloca os 𝜙(𝑥#, 𝑥$) em cada arvore.
-	public MRFTree(Tree arvore, Dataset ds, int c) {
-		ArrayList<int[]> tfiber = ds.Fiber(c);
-		n = tfiber.get(0).length;
+	public MRFTree(Tree arvore, Dataset ds, int c) { // trocar para apenas receber o dataset tfiber
+		Dataset tfiber = ds.Fiber(c);
+		n = tfiber.getN();
+		mc = tfiber.data.size();
+		
 		D = ds.getVar();
 		this.ds = ds;
 		
 		WeightedTree markovtree = new WeightedTree(n);
-		e = new int[2];
+		
+		e = new int[2]; //aresta especial
 		e[0] = 0;
 		e[1] = 0;
+		
 		int d = 1;
 		while (e[1] == 0) {
-			if (arvore.EdgeQ(0, d)) e[1] = d;
+			if (arvore.EdgeQ(0, d)) e[1] = d; // a aresta especial e a aresta que liga o no 0 a outra no (sendo este o no minimo...)
 			d++;
 		}
+		
+		// adicionamos os phi a cada aresta da nova arvore
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				markovtree.Add(i, j, phi(i,j)); // i j?
@@ -48,24 +55,29 @@ public class MRFTree {
 		return m;
 	}
 	
-	public double[][] phi(int k, int l) { 
-		double[][] r = new double[n][n];
-		if (e[0] == k && e[1] == l) { // verifica se esta e a aresta especial
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					int[] vars = {0, 0};
-					int[] vals = {0, 0};					         ;
-					r[k][l] = (ds.Count(vars, vals) + delta) / (mc + delta * D[i] * D[j]);
+	public double[][] phi(int i, int j) {  // calculo do phi
+		double[][] phiv = new double[n][n];
+		
+		if (i == e[0] && j == e[1]) { // verifica se esta e a aresta especial
+			for (int xi = 0; xi < n; xi++) {
+				for (int xj = 0; xj < n; xj++) {
+					int[] vars = {i, j};
+					int[] vals = {xi, xj};					         ;
+					phiv[xi][xj] = (tfiber.Count(vars, vals) + delta) / (mc + delta * D[i] * D[j]);
 				}
 			}
-		} else {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					r[k][l] = (count + delta) / (mc + delta * D[i] * D[j]);
+		} else { // esta nao e a aresta especial
+			for (int xi = 0; xi < n; xi++) {
+				for (int xj = 0; xj < n; xj++) {
+					int[] vars = {i, j};
+					int[] vals = {xi, xj};	
+					int[] var = {i};
+					int[] val = {xi};
+					phiv[xi][xj] = (tfiber.Count(vars, vals) + delta) / (tfiber.Count(var, val) + delta * D[j]);
 				}
 			}
 		}
-		return r; // TODO, não está feito
+		return phiv; // TODO, não está feito
 	}
 	
 	
